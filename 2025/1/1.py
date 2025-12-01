1879be034d8324519a6deeea984a53e788841a67
Max L. Wilson <max.wilson@nottingham.ac.uk>
2025-12-01 18:22:33 +0000

    2025 day 1 part 2, naive loop example

--- 2025/1/1.py
+++ 2025/1/1.py
@@ -1,23 +1,35 @@
 with open('1.in') as f:
     dataset = f.readlines()
     dataset = [line.rstrip('\n') for line in dataset]
-
+DEBUG = True
 start = 50
-zeros = 0
+zerostotal = 0
+
 for row in dataset:
+    zeros = 0
     dir = row[0]
     count = int(row[1:])
     if dir == "L":
+        was = start
+        if DEBUG: print(start,"-",count)
         start = start - count
         while start < 0:
             start = start + 100
+            zeros = zeros + 1
+        if start == 0:
+            zeros = zeros + 1
+        if DEBUG: print("=", start)
+        if was == 0:
+            zeros = zeros -1
+        if DEBUG: print(zeros, "x 0\n----\n")
     else:
-        start = start + count
+        if DEBUG: print(start,"+",count)
+        start = start + count   
         while start >= 100:
             start = start - 100
-    if start == 0:
-        zeros = zeros + 1
+            zeros = zeros + 1
+        if DEBUG: print("=", start)     
+        if DEBUG: print(zeros, "x 0\n----\n")
+    zerostotal = zerostotal + zeros
 
-print("answer is ", zeros)
-    
-
+print("answer is ", zerostotal)
