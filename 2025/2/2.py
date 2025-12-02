55da52ddcd887b79b48917a72f2952bb7aa9b253
Max L. Wilson <max.wilson@nottingham.ac.uk>
2025-12-02 11:30:12 +0000

    2025 day 2 part 2

--- 2025/2/2.py
+++ 2025/2/2.py
@@ -1,22 +1,30 @@
 import math
-with open('2.test') as f:
+with open('2.in') as f:
     dataset = f.readline()
 
 ranges = dataset.split(",")
 answer = 0
 for range in ranges:
+    # print("doing range",range)
     pidmin,pidmax = range.split("-")
-    if len(pidmin) > 1:
-        pidminleft = pidmin[0:math.floor(len(pidmin)/2)]
-    else:
-        pidminleft = "1"
-        
-    testval = int(pidminleft + pidminleft)
-    while testval <= int(pidmax):
-        if len(str(testval)) >= len(str(pidmin)):
-            if testval >= int(pidmin) and testval <= int(pidmax):
-                answer = answer + testval
-        pidminleft = str(int(pidminleft) + 1)
-        testval = int(pidminleft + pidminleft)
+    pidminleft = 1
+    found = [] #used so that we dont find 1111 because of 1x4 and 11x2
 
+    #for all numbers where at least doubling it is <= the length of the range maximum
+    while (len(str(pidminleft))*2 <= len(pidmax)):
+        testval = str(pidminleft)
+        #for all versions of that number up until its too long (1, 11, 111, 1111)
+        while len(testval) < len(pidmax):
+            #add a dup (1, 11, 111, 1111) and make a it a number for testing
+            testval = int(testval + str(pidminleft))
+            #if not aleady found and in range
+            if testval not in found and testval >= int(pidmin) and testval <= int(pidmax):
+                answer = answer + testval
+                found.append(testval)
+                # print("found",testval)
+            #make str again for next copy (1, 11, 111, 1111)
+            testval = str(testval)
+        #next number
+        pidminleft = pidminleft + 1
+    # temp = input() #just so i can check the solution one range at a time
 print("answer is",answer)
--- 2025/README.md
+++ 2025/README.md
@@ -4,7 +4,7 @@
 | Day | Part 1           | Part 2           |
 |-----|------------------|------------------|
 | 1 | [:star:](https://github.com/drmaxlwilson/adventofcode/commit/a5561c7159ed29ac9ab42888bff15e6a071e5771) | [:star:](1/1.py) |
-| 2 | [:star:](2/2.py) |  |
+| 2 | [:star:](https://github.com/drmaxlwilson/adventofcode/commit/ecb7601bc7a2b727e7f01f0e6fac68aba012674a) | [:star:](2/2.py) |
 | 3 |  |  |
 | 4 |  |  |
 | 5 |  |  |
