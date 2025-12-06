c04df6050e8ec36a18e7a77049f3175b29a709d9
Max L. Wilson <max.wilson@nottingham.ac.uk>
2025-12-06 18:17:52 +0000

    added checks to the filemaking scripts

--- makeday.py
+++ makeday.py
@@ -3,10 +3,17 @@ import os
 from pathlib import Path
 import shutil
 
+allowedlanguages = ["python"]
+
+if (len(sys.argv) != 4):
+    sys.exit("ERROR: requires argv: year day language")
+
 year = sys.argv[1]
 day = sys.argv[2]
 language = sys.argv[3]
 
+if language not in allowedlanguages:
+    sys.exit("ERROR: allowed languages are: "+", ".join(allowedlanguages))
 
 cwd = os.getcwd()
 if not os.path.exists(cwd+"/"+year):
@@ -18,4 +25,7 @@ Path(cwd+"/"+year+"/"+day+"/"+day+".in").touch()
 Path(cwd+"/"+year+"/"+day+"/"+day+".test").touch()
 
 if language == "python":
-    shutil.copyfile(cwd+"/template.py",cwd+"/"+year+"/"+day+"/"+day+".py")
+    if not os.path.exists(cwd+"/"+year+"/"+day+"/"+day+".py"):
+        shutil.copyfile(cwd+"/template.py",cwd+"/"+year+"/"+day+"/"+day+".py")
+    else:
+        sys.exit("ERROR: "+cwd+"/"+year+"/"+day+"/"+day+".py already exists")
--- makeyear.py
+++ makeyear.py
@@ -1,9 +1,22 @@
 import sys
 import os
 
+allowedlanguages = ["python"]
+
+if (len(sys.argv) != 4):
+    sys.exit("ERROR: requires argv: year numdays language")
+
 year = sys.argv[1]
 days = sys.argv[2]
 language = sys.argv[3]
+
+if language not in allowedlanguages:
+    sys.exit("ERROR: allowed languages are: "+", ".join(allowedlanguages))
+
+try:
+    days = int(days)
+except ValueError:
+    sys.exit("ERROR: numdays needs to be a number")
 
 for day in range(1,int(days)+1):
     os.system("python3 makeday.py "+year+" "+str(day)+" "+language)
