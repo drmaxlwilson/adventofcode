bb0cb6993d9f193e599ce46d1726f14708875ebf
Max L. Wilson <max.wilson@nottingham.ac.uk>
2025-12-06 09:58:19 +0000

    2025 day 6 part 2

--- 2025/6/6.py
+++ 2025/6/6.py
@@ -16,18 +16,44 @@ def part1():
                 val1 = val1 + int(val)
             elif op == "*":
                 val1 = val1 * int(val)
-            else:
-                temp = input("not * or +",op)
-        
         answertotal = answertotal + val1
     return answertotal    
+
+def part2():
+    answertotal = 0
+    count = 1 #these next two lines counter for the blank vertical line in all the subsequent math
+    vals = [""]
+    for x in range(len(dataset[0])-1,-1,-1):
+        vals.append("")
+        for y in range(0,len(dataset)):
+            vals[count] = vals[count] + dataset[y][x]
+            if dataset[y][x] == "+" or dataset[y][x] == "*":
+                answertotal = answertotal + dosum(vals,dataset[y][x])
+                vals = []
+                count = -1
+        count = count + 1
+    return answertotal
+            
+def dosum(vals,op):
+    val1 = int(vals[1]) #ignore the first blank column
+    for x in range(2,len(vals)):
+        if op == "+":
+            val1 = val1 + int(vals[x][:-1])
+        elif op == "*":
+            val1 = val1 * int(vals[x][:-1])
+    return val1
+        
 
 t0 = datetime.datetime.now()
 answer = part1()
 print("part 1",answer)
 t1 = datetime.datetime.now()
+answer = part2()
+print("part 2",answer)
+t2 = datetime.datetime.now()
 
 td1 = t1-t0
+td2 = t2-t1
 print("Part 1 time: {} ms".format(td1.microseconds/1000))
-
+print("Part 1 time: {} ms".format(td2.microseconds/1000))
 
